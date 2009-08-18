edf404061b7100da253b14735cfd7c3edaf1bea2
yogarine <yogarine@9710c022-dddb-11dd-993c-4d20d8c3c59d>
2009-08-18 23:46:15 +0000

    Won't need the TwoWayScrollView anymore.

--- src/com/yogarine/tacsoc/TacSoc.java
+++ src/com/yogarine/tacsoc/TacSoc.java
@@ -7,18 +7,15 @@ import android.widget.LinearLayout.LayoutParams;
 
 public class TacSoc extends Activity {
 
-	static int FILL_PARENT = LinearLayout.LayoutParams.FILL_PARENT;
-	static int WRAP_CONTENT = LinearLayout.LayoutParams.WRAP_CONTENT;
+	public static final int FILL_PARENT = LinearLayout.LayoutParams.FILL_PARENT;
+	public static final int WRAP_CONTENT = LinearLayout.LayoutParams.WRAP_CONTENT;
 	private LayoutParams _layoutParams;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		_layoutParams = new LayoutParams(FILL_PARENT,FILL_PARENT);
-//		TwoWayScrollView twoWayScrollView = new TwoWayScrollView(this);
 		GameView gameView = new GameView(this);
-//		twoWayScrollView.addView(gameView,_layoutParams);
-//		setContentView(twoWayScrollView,_layoutParams);
 		setContentView(gameView,_layoutParams);
 	}
 
