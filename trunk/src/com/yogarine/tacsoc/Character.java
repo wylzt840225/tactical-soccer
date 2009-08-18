c6fd5774973a1707bb7f20b776c6dce3e3430b39
yogarine <yogarine@9710c022-dddb-11dd-993c-4d20d8c3c59d>
2009-08-18 23:59:08 +0000

    Added character class stub.

---
+++ trunk/src/com/yogarine/tacsoc/Character.java
@@ -0,0 +1,17 @@
+package com.yogarine.tacsoc;
+
+import android.view.GestureDetector.SimpleOnGestureListener;
+
+public class Character extends SimpleOnGestureListener {
+	
+	public int hp;
+	public int mp;
+	public int str;
+	public int vit;
+	public int sta;
+
+	public Character() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
